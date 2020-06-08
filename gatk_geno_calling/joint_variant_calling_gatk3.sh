f5ab56cef6b5a32de8a80f8951d2ddf80a71d183
“burook.misganaw@gmail.com” <burook.misganaw@gmail.com>
2020-06-07 22:19:34 -0400

    updated exome interval file

--- gatk_geno_calling/joint_variant_calling_gatk3.sh
+++ gatk_geno_calling/joint_variant_calling_gatk3.sh
@@ -21,12 +21,15 @@ java -Xmx200g -jar /apps/software/GATK/3.8-0-Java-1.8.0_161/GenomeAnalysisTK.jar
     -R ${refloc1}/genome.fa \
     -V ${dir1}/list_VCF1.list \
     --dbsnp ${refloc1}/dbsnp_135.hg19.vcf \
-    --disable_auto_index_creation_and_locking_when_reading_rods \
-    --intervals exome2.bed \
-    --out SysBio_exome_merged11.vcf
+    --intervals exome3.bed \
+    --out SysBio_exome_merged12.vcf
 
 
+#    --disable_auto_index_creation_and_locking_when_reading_rods \
+#   --intervals exome3.bed \
+#   --disable_auto_index_creation_and_locking_when_reading_rods \
 #   -nt 8 \
 #     
+
 
  
